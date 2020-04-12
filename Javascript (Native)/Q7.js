268b6f8fe1f21991a5712228fe9c59cb446b7315
DhanushM12 <dhanushmukhi@gmail.com>
2020-04-12 17:38:35 +0530

    Question 7 completed

--- Javascript (Native)/Q7.js
+++ Javascript (Native)/Q7.js
@@ -1,1 +1,34 @@
 // Q7. Give a relevant example of Promises
+1. A Promise is in one of these states:
+
+pending: initial state, neither fulfilled nor rejected.
+fulfilled: meaning that the operation completed successfully.
+rejected: meaning that the operation failed.
+
+2. As the Promise.prototype.then() and Promise.prototype.catch() methods return promises
+
+3. Chained Promises
+
+const myPromise =
+  (new Promise(myExecutorFunc))
+  .then(handleFulfilledA)
+  .then(handleFulfilledB)
+  .then(handleFulfilledC)
+  .catch(handleRejectedAny);
+
+  4. Another example
+   let myFirstPromise = new Promise((resolve, reject) => {
+    // We call resolve(...) when what we were doing asynchronously was successful, and reject(...) when it failed.
+    // In this example, we use setTimeout(...) to simulate async code. 
+    // In reality, you will probably be using something like XHR or an HTML5 API.
+    setTimeout( function() {
+      resolve("Success!")  // Yay! Everything went well!
+    }, 250) 
+  }) 
+  
+  myFirstPromise.then((successMessage) => {
+    // successMessage is whatever we passed in the resolve(...) function above.
+    // It doesn't have to be a string, but if it is only a succeed message, it probably will be.
+    console.log("Yay! " + successMessage) 
+  });
+  
