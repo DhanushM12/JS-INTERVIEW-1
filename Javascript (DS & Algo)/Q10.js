29a7d3a060f0c8a715e21c4eba467d04909cb2cf
DhanushM12 <dhanushmukhi@gmail.com>
2020-04-12 18:30:07 +0530

    Question 10 completed

--- Javascript (DS & Algo)/Q10.js
+++ Javascript (DS & Algo)/Q10.js
@@ -1,1 +1,80 @@
 // Q10. Implement LinkedList class in JS.
+//creating node class
+class Node {
+  // constructor
+  constructor(element) {
+    this.element = element;
+    this.next = null;
+  }
+}
+//creating linked list class
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  // functions to be implemented
+  // add(element)
+  // adds the element to linked list
+  push(element) {
+    // creates a new node
+    var node = new Node(element);
+
+    // to store current node
+    var current;
+
+    // if list is Empty add the
+    // element and make it head
+    if (this.head == null) this.head = node;
+    else {
+      current = this.head;
+
+      // iterate to the end of the
+      // list
+      while (current.next) {
+        current = current.next;
+      }
+
+      // add node
+      current.next = node;
+    }
+    this.size++;
+  }
+  // removeElement(element)
+  // removes a given element from the linked list
+  removeElement(element) {
+    var current = this.head;
+    var prev = null;
+
+    // iterate over the list
+    while (current != null) {
+      // comparing element with current
+      // element if found then remove the
+      // and return true
+      if (current.element == element) {
+        if (prev == null) {
+          this.head = current.next;
+        } else {
+          prev.next = current.next;
+        }
+        this.size--;
+        return current.element;
+      }
+      prev = current;
+      current = current.next;
+    }
+    return -1;
+  }
+
+  //Print linked list traversing through the list
+  printList() {
+    var curr = this.head;
+    var str = "";
+    while (curr) {
+      str += curr.element + " ";
+      curr = curr.next;
+    }
+    console.log(str);
+  }
+}
